41eb6cbee56f53485b8c4a57dc22e12270c79bed
mmhvyc <catscrossfitcomputers@gmail.com>
2020-11-01 22:13:23 -0600

    Added animal, contact, and provider types

---
+++ Whisker/src/app/types/provider.ts
@@ -0,0 +1,12 @@
+export interface Provider {
+    rescue: string,
+    street: string,
+    city: string,
+    state: string,
+    zip: number,
+    country: string,
+    website: string,
+    email: string,
+    phone: number,
+    available: string[]
+}
